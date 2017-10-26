b2592105b6e8023598bb26fe0db865cde55cbadc
EpicIvo <ivo@burst-digital.com>
2017-10-26 16:13:29 +0200

    Added ✨F✨A✨N✨C✨Y✨ animations!

---
+++ src/components/App/styled/styles.js
@@ -0,0 +1,44 @@
+import glamorous from 'glamorous';
+
+const Title = glamorous.h1({
+  fontFamily: 'Avenir Next',
+  fontSize: '30pt',
+  textAlign: 'center',
+  width: '100%',
+  color: '#2e2e2e',
+  marginTop: '2vh',
+});
+
+const DoubleBlockContainer = glamorous.div({
+  textAlign: 'center',
+});
+
+const BlockContainer = glamorous.div({
+  width: '50%',
+  padding: '0 0 50px 0',
+  display: 'inline-block'
+});
+
+const Block = glamorous.div({
+  fontFamily: 'Avenir Next',
+  fontSize: '40pt',
+  display: 'inline-block',
+  backgroundColor: '#2e2e2e',
+  color: '#ffffff',
+  width: '80%',
+  height: '40vh',
+  lineHeight: '40vh',
+  textAlign: 'center',
+  borderRadius: '25px',
+  border: '2px solid #38FF87',
+  overflow: 'hidden',
+  transitionDuration: '0.3s',
+  ':hover': {
+    backgroundColor: '#38FF87',
+    border: '2px solid #2e2e2e',
+    color: '#2e2e2e',
+    transform: 'Scale(1.1)',
+  }
+});
+
+export {Title, DoubleBlockContainer, BlockContainer, Block};
