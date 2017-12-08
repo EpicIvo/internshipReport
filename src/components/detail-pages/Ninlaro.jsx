e210ce42a40233f238f6769f9db7d051b1ae02ba
EpicIvo <ivo@burst-digital.com>
2017-12-08 14:25:50 +0100

    Made it all a bit more dynamic

--- src/components/detail-pages/Ninlaro.jsx
+++ src/components/detail-pages/Ninlaro.jsx
@@ -5,10 +5,46 @@ import {Title, DetailPageTopBar, Content, SubTitle} from './../../shared/detailS
 import {CategoryItem} from './../App/styles/styles.js';
 // Components
 import BackButton from '../common/BackButton/BackButton.jsx';
+//data
+import dataObject from '../../data/data.js';
 
 class Ninlaro extends React.Component {
   constructor() {
     super();
+    this.state = {
+      title: dataObject.projects.ninlaro.title,
+      content: dataObject.projects.ninlaro.content,
+      subTitles: dataObject.projects.ninlaro.subTitles,
+      categories: dataObject.projects.ninlaro.categories,
+    };
+    this.renderCategory = this.renderCategory.bind(this);
+  }
+
+  renderCategory() {
+    return this.state.categories.map(category => {
+      return <Link key={category.title} to={category.path}>
+        <CategoryItem>
+          {category.title}
+        </CategoryItem>
+      </Link>
+    });
+  }
+
+  renderContent() {
+    return this.state.content.map(item => {
+      if (item.type === 'paragraph') {
+        return <Content key={item.content}>
+          {item.content}
+          <br/><br/>
+        </Content>;
+      } else if (item.type === 'subTitle') {
+        return <SubTitle key={item.content} ninlaro>
+          >_ {item.content}
+        </SubTitle>
+      } else {
+        return null
+      }
+    });
   }
 
   render() {
@@ -16,48 +52,12 @@ class Ninlaro extends React.Component {
       <div>
         <Title ninlaro>
           <BackButton icon="<" path="/"/>
-          Ninlaro
+          {this.state.title}
         </Title>
         <DetailPageTopBar>
-          <Link to="/ontwikkelend">
-            <CategoryItem>
-              Ontwikkelend
-            </CategoryItem>
-          </Link>
-          <Link to="/betrokken">
-            <CategoryItem>
-              Betrokken
-            </CategoryItem>
-          </Link>
-          <Link to="/ondernemend">
-            <CategoryItem>
-              Ondernemend
-            </CategoryItem>
-          </Link>
+          {this.renderCategory()}
         </DetailPageTopBar>
-
-        <Content>
-          Oorspronkelijk stond ik maar twee dagen ingepland op Ninlaro, dit werden alleen wel wat meer dagen na wat misverstanden.
-          Aan mij was de taak gegeven ervoor te zorgen dat alle content dat geüpload moest worden op de goede plek stond en ook echt
-          goed geüpload werd. Ik moest dit doen aangezien Roël en Daan die de content hebben geüpload beide op vakantie gingen.
-          <br/><br/>
-          De klant had in een google drive folder alle content gezet met daarbij informatie over waar het moest komen te staan. Dit was
-          de data dat ik gebruikte om te checken of alles goed stond. En na dit alles te zijn doorgelopen leek het allemaal zeker goed te
-          staan.
-        </Content>
-        <SubTitle ninlaro>
-          >_ Meeting
-        </SubTitle>
-        <Content>
-          De projectmanager met wie ik dit project samen deed was de maandag waarop de klant voor een opleveringsmeeting kwam niet aanwezig. In
-          het weekend vroeg ze of ik dit op mij wilde nemen aangezien er niemand anders (die bij het project betrokken was geweest) op kantoor
-          zou zijn. Ik had de website al aardig doorgenomen tegen deze tijd en wist dus dat ik de klant wel goed te woord kon staan.
-          <br/><br/>
-          Tegen het eind van de middag kwamen twee
-        </Content>
-        <SubTitle ninlaro>
-          >_ Subtitle
-        </SubTitle>
+        {this.renderContent()}
       </div>
     )
   }
