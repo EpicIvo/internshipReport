ebd33f961b26ea80488e7d7ec9a4cebf56c8c869
EpicIvo <ivo@burst-digital.com>
2018-03-11 14:33:21 +0100

    feature(build): update build environment to be static

--- kyt.config.js
+++ kyt.config.js
@@ -1,7 +1,18 @@
-// Base kyt config.
-// Edit these properties to make changes.
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   reactHotLoader: true,
   debug: false,
+  hasServer: false,
+  modifyWebpackConfig: (config, options) => {
+    if (options.type === 'client') {
+      config.plugins.push(new HtmlWebpackPlugin({
+        template: 'src/index.ejs',
+      }));
+    }
+
+    config.resolve.extensions.push('.jsx');
+
+    return config;
+  },
 };
